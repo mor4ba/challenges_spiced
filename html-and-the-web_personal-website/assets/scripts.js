c3c97242361862fa6f4180065cf1f6ca399d4fa0
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-02-16 17:30:07 +0100

    add JavaScript for smoothscroll

--- html-and-the-web_personal-website/assets/scripts.js
+++ html-and-the-web_personal-website/assets/scripts.js
@@ -1,4 +1,7 @@
-const expandTrigger = document.getElementById("expandTrigger");
+addEventListener("DOMContentLoaded", (event) => {
+  const expandTrigger = document.getElementById("expandTrigger");
+  negativeMouse();
+});
 
 expandTrigger.addEventListener("click", () => {
   event.preventDefault();
@@ -22,3 +25,39 @@ function expandAccordion() {
     this.trigger.parentElement.classList.remove("bg-bright");
   }
 }
+
+function negativeMouse() {
+  var $c = document.querySelector("[data-custom-cursor]");
+  var $t = document.querySelectorAll(".negative-scope");
+
+  if (!$t) {
+    return;
+  }
+
+  window.addEventListener("mousemove", function (e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    $c.style.transform = "matrix(1, 0, 0, 1, " + x + "," + y + "";
+  });
+
+  $t.forEach((element) => {
+    element.addEventListener("mouseenter", function () {
+      $c.classList.add("custom-cursor-active");
+    });
+
+    element.addEventListener("mouseleave", function () {
+      $c.classList.remove("custom-cursor-active");
+    });
+  });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    var element = document.querySelector(this.getAttribute("href"));
+    const y = element.getBoundingClientRect().top + window.pageYOffset - 50;
+
+    window.scrollTo({ top: y, behavior: "smooth" });
+  });
+});
