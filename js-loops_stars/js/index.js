17ca7112f5f319617669deebe2935c13e5ac07d9
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-03-03 16:11:43 +0100

    loops challenges + solution

---
+++ js-loops_stars/js/index.js
@@ -0,0 +1,27 @@
+console.clear();
+
+const starContainer = document.querySelector('[data-js="star-container"]');
+
+function renderStars(filledStars) {
+  // reset the star container before rerendering stars
+  starContainer.innerHTML = "";
+
+  //--v-- your code here --v--
+
+  for (let i = 1; i <= 5; i++) {
+    let img = document.createElement("img");
+    if (i <= filledStars) {
+      img.setAttribute("src", "assets/star-filled.svg");
+    } else {
+      img.setAttribute("src", "assets/star-empty.svg");
+    }
+
+    img.addEventListener("click", () => {
+      renderStars(i);
+    });
+    starContainer.append(img);
+  }
+  //--^-- your code here --^--
+}
+
+renderStars();
